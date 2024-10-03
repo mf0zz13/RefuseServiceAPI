de345ffb506e250a4edad5d53fe99970c9988422
mf0zz13 <michael.foster31@outlook.com>
2024-10-03 14:27:44 -0400

    ## Added * Constructor for DispatchRecordController class * GetDispatchRecordsFromToday method ## Modified * GetAllDispatchRecords method

--- Controllers/DispatchRecordController.cs
+++ Controllers/DispatchRecordController.cs
@@ -2,6 +2,7 @@
 using MySqlConnector;
 using Dapper;
 using DispatchRecordAPI.Models;
+using System.Collections.Generic;
 
 namespace DispatchRecordAPI.Controllers
 {
@@ -9,31 +10,64 @@ namespace DispatchRecordAPI.Controllers
     [Route("[controller]")]
     public class DispatchRecordController : ControllerBase
     {
-        [HttpGet]
-        //[Route("AllDispatchRecords")]
-        public List<DispatchRecord> GetAllDispatchRecords()
+        private MySqlConnection connection;
+
+        public DispatchRecordController(IConfiguration configuration)
         {
+            var password = configuration["Database:Password"];
             var builder = new MySqlConnectionStringBuilder
             {
                 Server = "mysqlmssa.mysql.database.azure.com",
                 Database = "guntherrefuse",
                 UserID = "applogin",
-                Password = "T3stP@ssw0rd",
+                Password = password,
                 SslMode = MySqlSslMode.Required,
             };
 
-            var connection = new MySqlConnection(builder.ConnectionString);
-
-            connection.Open();
-
-            return connection.Query<DispatchRecord>("SELECT * FROM DispatchRecords;").ToList();
+            connection = new MySqlConnection(builder.ConnectionString);
         }
 
-        //[HttpGet]
-        //[Route("AllDispatchRecordsFromToday")]
-        //public List<DispatchRecord>? GetDispatchRecordsFromToday()
-        //{
-        //    return connection.Query<DispatchRecord>($"SELECT * FROM DispatchRecords WHERE DispatchDate = {DateTime.Today.Year}-{DateTime.Today.Month}-{DateTime.Today.Day}").ToList();
-        //}
+        [HttpGet]
+        [Route("AllDispatchRecords")]
+        public List<DispatchRecord> GetAllDispatchRecords()
+        {
+            try
+            {
+                connection.Open();
+                return connection.Query<DispatchRecord>("SELECT * FROM DispatchRecords;").ToList();
+
+            }
+            catch
+            {
+                return new List<DispatchRecord>();
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        [HttpGet]
+        [Route("AllDispatchRecordsFromToday")]
+        public List<DispatchRecord>? GetDispatchRecordsFromToday()
+        {
+            try
+            {
+                connection.Open();
+                return connection.Query<DispatchRecord>($"SELECT * FROM DispatchRecords " +
+                                                        $"WHERE DispatchDate = {DateTime.Today.Year}-{DateTime.Today.Month}-{DateTime.Today.Day}"
+                                                        ).ToList();
+
+
+            }
+            catch
+            {
+                return new List<DispatchRecord>();
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
     }
 }

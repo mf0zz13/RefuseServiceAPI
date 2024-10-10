cf16a916b098f06b81d16cff22b4d777e164ef79
mf0zz13 <michael.foster31@outlook.com>
2024-10-09 20:47:47 -0400

    Added Employee Model

---
+++ Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace DispatchRecordAPI.Models
+{
+    public class Employee
+    {
+        public string EmployeeID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Role { get; set; }
+        public DateTime? CdlExpiration { get; set; }
+        public DateTime EmploymentStartDate { get; set; }
+        public DateTime? EmploymentEndDate { get; set; }
+    }
+
+}
